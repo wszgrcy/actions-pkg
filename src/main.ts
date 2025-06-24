b906363ed22d733ca4db0ff1e3382ce53f5b5a9b
chen <wszgrcy@gmail.com>
2025-06-24 14:01:24 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import * as core from '@actions/core'
-import { downloadZstd } from './download-release'
+// import { downloadZstd } from './download-release'
 import * as path from 'path'
 import * as fs from 'fs'
 import { rimraf } from 'rimraf'
+import { downloadFile } from 'ipull'
+import extract from 'extract-zip'
+import * as tar from 'tar'
+async function downloadZstd(options: {
+  tag: string
+  fileName: string
+  // outFilePath: string
+}) {
+  let tempPath = path.join(process.cwd(), '../temp')
+  let outputPath = path.join(process.cwd(), '../zstd-dir')
 
+  let url = `https://github.com/facebook/zstd/releases/download/${options.tag}/${options.fileName}`
+  console.log('准备下载', url)
+
+  await downloadFile({ url: url, savePath: tempPath })
+  console.log('下载完成', tempPath)
+  if (options.fileName.endsWith('.zip')) {
+    await extract(tempPath, { dir: outputPath })
+    console.log('解压完成', outputPath)
+  }
+  return outputPath
+}
 /**
  * The main function for the action.
  *
@@ -12,27 +33,28 @@ import { rimraf } from 'rimraf'
 export async function run(): Promise<void> {
   try {
     console.log('env', process.env)
-    // tar.c({strip:1}, ['eic']).pipe(fs.createWriteStream('xxx.tar'));
 
-    const zstdTag: string = core.getInput('zstdTag')
-    const zstdFileName: string = core.getInput('zstdFileName')
-    console.log(zstdTag)
-    console.log(zstdFileName)
-
-    await downloadZstd({ tag: zstdTag, fileName: zstdFileName })
+    let cwd = await downloadZstd({
+      tag: 'v1.5.7',
+      fileName: 'zstd-v1.5.7-win64.zip'
+    })
     console.log(process.cwd())
-    let zstdDir = path.join(process.cwd(), '../zstd')
-    console.log(zstdDir)
     // todo execa执行zstd
-    let result = fs.readdirSync(zstdDir)
+    let result = fs.readdirSync(cwd)
     console.log(result)
-    const cleanCwd = core.getInput('cleanCwd', { required: false })
-    if (cleanCwd) {
+    const dir = core.getInput('dir')
+
+    if (dir) {
       const cleanPaths = core.getInput('cleanPaths', { required: false })
       console.log('清理', cleanPaths)
 
-      // rimraf([],{glob:{cwd:cleanCwd}})
+      // rimraf([], { glob: { cwd: dir } })
     }
+    const outputPath = core.getInput('outputPath')
+    const absOutputPath = path.join(process.cwd(), outputPath)
+    tar.c({ sync: true }, [dir]).pipe(fs.createWriteStream(absOutputPath))
+    let res2 = fs.existsSync(absOutputPath)
+    console.log('是否存在', absOutputPath, res2)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
